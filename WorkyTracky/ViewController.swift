95a3e81d35be1b7f45693eb8a884447421f19ddf
Daiva Adisurya <daiva.adisurya@gmail.com>
2020-04-08 14:43:28 +0700

    Initial Commit

---
+++ WorkyTracky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkyTracky
+//
+//  Created by Daiva Adisurya on 08/04/20.
+//  Copyright © 2020 AppleAcademy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
