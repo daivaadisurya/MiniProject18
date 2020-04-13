25d19f3217cdf990fefe05cd7f479fe068d9d3da
rafikaalifia <62131104+rafikaalifia@users.noreply.github.com>
2020-04-13 11:27:53 +0700

    add: Detail Activity Storyboard and Detail Activity View Controller

---
+++ WorkyTracky/Controller/DetailActivityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailActivityViewController.swift
+//  WorkyTracky
+//
+//  Created by Rafika Alifia Isti on 13/04/20.
+//  Copyright © 2020 AppleAcademy. All rights reserved.
+//
+
+import UIKit
+
+class DetailActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
